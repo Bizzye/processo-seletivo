e52da0be050122a9002c00cb6abfed68ebf2137e
Daniel Bizarro <bizarro1306@gmail.com>
2021-04-11 01:10:28 -0300

    renomeando as variaveis para portugues

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   
-  countDownDate = new Date("Nov 26, 2021 23:59:59").getTime();
+  DataLimite = new Date("Nov 26, 2021 23:59:59").getTime();
   
   constructor() { }
 
@@ -18,22 +18,22 @@ export class MainComponent implements OnInit {
 // Update the count down every 1 second
   x = setInterval(() => {
 
-    var now = new Date().getTime();
-    var distance = this.countDownDate - now;
-      console.log(distance);
-      console.log(this.countDownDate);
-      console.log(now);
+    var agora = new Date().getTime();
+    var restante = this.DataLimite - agora;
+      console.log(restante);
+      console.log(this.DataLimite);
+      console.log(agora);
     // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let dias = Math.floor(restante / (1000 * 60 * 60 * 24));
+    let horas = Math.floor((restante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutos = Math.floor((restante % (1000 * 60 * 60)) / (1000 * 60));
+    let segundos = Math.floor((restante % (1000 * 60)) / 1000);
       
     // Output the result in an element with id="demo"
-    this.demo = { days, hours, minutes, seconds };
+    this.demo = { dias, horas, minutos, segundos };
       
     // If the count down is over, write some text 
-    if (distance < 0) {
+    if (restante < 0) {
       clearInterval(this.x);
       this.demo = "EXPIROU";
     }
