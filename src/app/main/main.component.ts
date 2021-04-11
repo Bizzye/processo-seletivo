46d8941e74f9a6c653f47bd91d426437b9bbf6d7
Daniel Bizarro <bizarro1306@gmail.com>
2021-04-11 01:12:54 -0300

    renomeando as variaveis para portugues

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -15,7 +15,7 @@ export class MainComponent implements OnInit {
   }
 
   demo:any;
-// Update the count down every 1 second
+
   x = setInterval(() => {
 
     var agora = new Date().getTime();
@@ -23,17 +23,15 @@ export class MainComponent implements OnInit {
       console.log(restante);
       console.log(this.DataLimite);
       console.log(agora);
-    // Time calculations for days, hours, minutes and seconds
+
     let dias = Math.floor(restante / (1000 * 60 * 60 * 24));
     let horas = Math.floor((restante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutos = Math.floor((restante % (1000 * 60 * 60)) / (1000 * 60));
     let segundos = Math.floor((restante % (1000 * 60)) / 1000);
       
-    // Output the result in an element with id="demo"
     this.demo = { dias, horas, minutos, segundos };
       
-    // If the count down is over, write some text 
-    if (restante < 0) {
+   if (restante < 0) {
       clearInterval(this.x);
       this.demo = "EXPIROU";
     }
